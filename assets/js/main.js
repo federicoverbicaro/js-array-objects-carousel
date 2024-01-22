e4f67280449fa85f1af49a5fd99eabcbc9f28663
federicoverbicaro <federico.verbicaro@icloud.com>
2024-01-22 18:46:55 +0100

    feat: Fine esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+const frecciaSinistraHtml = document.getElementById("frecciaSinistra")
+const frecciaDestraHtml = document.getElementById("frecciaDestra")
+
+
+
+let figureHtml = document.getElementById("figureImg");
+const mainImage = document.getElementById("mainImage");
+
+let indiceCorrente = 0;
+const intervalloTempo = 3000;
+
 
 const arrayImg = [
     './assets/img/NIke Mercurial Superfly 9-PhotoRoom.png-PhotoRoom.png',
@@ -8,11 +19,9 @@ const arrayImg = [
 
     './assets/img/NIke Tiempo legend 9 -PhotoRoom.png-PhotoRoom.png',
 
-    './assets/img/Nike Jr.mercurial -PhotoRoom.png-PhotoRoom.png'
+    './assets/img/Nike Jr.mercurial -PhotoRoom.png-PhotoRoom.png',
 ]
 
-let figureHtml = document.getElementById("figureImg");
-const mainImage = document.getElementById("mainImage");
 
 for (let i = 0; i < arrayImg.length; i++) {
 
@@ -21,7 +30,41 @@ for (let i = 0; i < arrayImg.length; i++) {
       alt="immagine scarpa da calcio">
 </figure>`
 
+
 }
+
+function visualizzaImmagine(indice) {
+    const oggettoScarpa = arrayDettagliScarpa[indice];
+
+    mainImage.innerHTML = `
+        <div class="col-10 p-0 d-flex m-0 w-100 flex-column">
+            <div class="d-flex p-3">
+                <h1 id="nomeScarpa" class="text-capitalize text-white">${oggettoScarpa.nome}</h1>
+            </div>
+            <figure class="align-self-center d-flex mt-5">
+                <img id="immagineScarpa" src="${oggettoScarpa.immagine}" class="d-block" alt="immagine scarpa da calcio">
+            </figure>
+            <div class="p-3 w-75 mb-5">
+                <p id="descrizioneScarpa" class="text-white text-capitalize">${oggettoScarpa.descrizione}</p>
+            </div>
+        </div>`;
+}
+function cambiaImmagineSuccessiva() {
+    indiceCorrente = (indiceCorrente + 1) % arrayDettagliScarpa.length;
+    visualizzaImmagine(indiceCorrente);
+}
+
+const intervalloSlideshow = setInterval(cambiaImmagineSuccessiva, intervalloTempo);
+
+frecciaSinistraHtml.addEventListener('click', function (event) {
+    indiceCorrente = (indiceCorrente - 1 + arrayDettagliScarpa.length) % arrayDettagliScarpa.length;
+    visualizzaImmagine(indiceCorrente);
+});
+
+frecciaDestraHtml.addEventListener('click', function (event) {
+    indiceCorrente = (indiceCorrente + 1) % arrayDettagliScarpa.length;
+    visualizzaImmagine(indiceCorrente);
+});
 
 
 
@@ -38,12 +81,12 @@ const arrayDettagliScarpa = [
 
     },
     {
-        nome: 'NIke Phatom GX',
+        nome: 'Nike Phatom GX',
         descrizione: "Fai sul serio? La scarpa da calcio Pro è perfetta per superare le difese con tiri a giro chirurgici, ma anche per dribbling imprendibili all'insegna della creatività.",
         immagine: './assets/img/NIke Phatom GX-PhotoRoom.png-PhotoRoom.png',
     },
     {
-        nome: 'NIke Tiempo legend 9',
+        nome: 'Nike Tiempo legend 9',
         descrizione: "1 delle Tiempo più leggere mai realizzati, Nike Tiempo Legend 9 Elite SG-Pro AC è perfetta per dare il massimo in campo, grazie al design a basso profilo, reinventato per gli attaccanti",
         immagine: './assets/img/NIke Tiempo legend 9 -PhotoRoom.png-PhotoRoom.png',
     },
@@ -54,6 +97,7 @@ const arrayDettagliScarpa = [
     },
 
 ]
+
 
 
 figureHtml.addEventListener("mouseover", function (event) {
@@ -69,18 +113,23 @@ figureHtml.addEventListener("mouseover", function (event) {
     }
 
     if (oggettoScarpa) {
-        mainImage.innerHTML = ` <div class="col-10 p-0 d-flex m-0 w-100 flex-column">
+        mainImage.innerHTML = ` 
+        <div class="col-10 p-0 d-flex m-0 w-100 flex-column">
+
         <div class="d-flex p-3">
           <h1 id="nomeScarpa" class="text-capitalize text-white">${oggettoScarpa.nome}</h1>
         </div>
-        <figure class="align-self-center d-flex">
+
+        <figure class="align-self-center d-flex mt-5 ">
           <img id="immagineScarpa" src="${oggettoScarpa.immagine}" class="d-block" alt="immagine scarpa da calcio">
         </figure>
-        <div class="p-3 w-75">
-          <p id="descrizioneScarpa" class="text-white text-capitalize">${oggettoScarpa.descrizione}</p>
+
+        <div class="p-3 w-75 mb-5">
+          <p id="descrizioneScarpa" class="text-white text-capitalize ">${oggettoScarpa.descrizione}</p>
         </div>
+
       </div>`;
-    } 
+    }
 
 })
 
